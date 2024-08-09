472cce02685bda5d5bc0a13424a7ea7c70be63a0
selva <selvakumarj1807@gmail.com>
2024-08-09 08:29:29 +0530

    Routes

--- resources/views/cantact.blade.php
+++ resources/views/cantact.blade.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Page</title>
+    <title>Cantact Page</title>
 </head>
 
 <body>
     <center>
-        Welcome Page
+        Cantact Page
     </center>
 
     <hr>
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('/home', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/cantact', function () {
+    return view('cantact');
+});
