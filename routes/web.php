aa8105c919c71d84463b4eb85622727fb852a53b
selva <selvakumarj1807@gmail.com>
2024-08-12 06:28:02 +0530

    creating controller

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,28 @@
+<!--
+Creating Controller :
+---------------------
+
+    1. php artisan make:controller StudentController
+
+
+    result :
+    --------
+
+        Controller [C:\Users\Selva\OneDrive\Desktop\Laravel_Project_01\student-reg\app\Http\Controllers\StudentController.php] created successfully.
+-->
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+use  App\Http\Controllers\StudentController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/home', [StudentController::class, 'viewForm']);
 
 Route::get('/about', function () {
     return view('about');
