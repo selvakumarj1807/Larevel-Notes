d550413dd9bf77cd0cd8f26a0ddb63c375955d10
selva <selvakumarj1807@gmail.com>
2024-08-13 11:10:06 +0530

    database setup

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,26 @@ Creating Controller :
     --------
 
         Controller [C:\Users\Selva\OneDrive\Desktop\Laravel_Project_01\student-reg\app\Http\Controllers\StudentController.php] created successfully.
+
+Database Setup :
+----------------
+
+    Table Creation :
+    ----------------
+
+        1. php artisan make:migration create_students_table
+
+    Table Migrate :
+    ---------------
+
+            Add the column in migrations file and run the below command for migration.
+
+        1. php artisan migrate
+
+    Table Migrate Refresh:
+    ----------------------
+
+        1. php artisan migrate:fresh
 -->
 
 <?php
